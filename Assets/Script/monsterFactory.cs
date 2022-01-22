e09319248323cf113993ab10892c01e61adcb698
YuThrones <YuThrones@users.noreply.github.com>
2022-01-22 21:50:02 +0800

    怪物球生成器

---
+++ Assets/Script/monsterFactory.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using gameEnum;
+
+public class monsterFactory : MonoBehaviour
+{
+
+    public float yOffset;
+    public GameObject monsterPrefab;
+    private bool isCreate;
+    void Start()
+    {
+        isCreate = false;
+    }
+
+    // Update is called once per frame
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!isCreate)
+        {
+            if (collision.gameObject.tag == "Ball")
+            {
+                Vector3 monsterPos = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + yOffset, gameObject.transform.position.z);
+                GameObject.Instantiate(monsterPrefab, monsterPos, Quaternion.identity);
+                isCreate = true;
+            }
+        }
+        
+    }
+
+    void FixedUpdate()
+    {
+    }
+}
