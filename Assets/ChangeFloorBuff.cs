8599774fda28bcb3b2d796935b93ae0e187b4793
YuThrones <YuThrones@users.noreply.github.com>
2022-01-22 16:34:35 +0800

    切平台buff

---
+++ Assets/ChangeFloorBuff.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using gameEnum;
+
+public class ChangeFloorBuff : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float effectRadius;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Ball"))
+        {
+            GameObject[] floors = GameObject.FindGameObjectsWithTag("Floor");
+            foreach(GameObject floor in floors)
+            {
+                if (Vector2.Distance(gameObject.transform.position, floor.transform.position) < effectRadius)
+                {
+                    floor.GetComponent<Floor>().color = GameColor.None;
+                }
+            }
+            Destroy(gameObject);
+        }
+    }
+}
